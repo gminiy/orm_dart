758bcfb8904c49f5c47c44f47b4df3cab9666c9d
gminiy <gmini.y@gmail.com>
2023-12-01 14:23:07 +0900

    연습문제 1-3

--- lib/23_11_30/cleric.dart
+++ lib/23_11_30/cleric.dart
@@ -6,4 +6,21 @@ class Cleric {
   String name;
 
   Cleric(this.name);
+
+  void selfAid() {
+    if (mp < 5) {
+      print("MP 가 부족합니다.");
+      return;
+    }
+    mp -= 5;
+    hp = maxHp;
+  }
+}
+
+void main() {
+  Cleric cleric = Cleric("Gmin");
+  cleric.hp = 10;
+  cleric.selfAid();
+  print(cleric.hp);
+  print(cleric.mp);
 }
