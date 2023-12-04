32da5aaa3601fe0222d93055d007fa6dd929ee6e
gminiy <gmini.y@gmail.com>
2023-12-04 14:55:12 +0900

    연습문제 10-1

---
+++ lib/23_12_4/wizzard.dart
@@ -0,0 +1,21 @@
+class Wand {
+  String name;
+  double power;
+
+  Wand({
+    required this.name,
+    required this.power,
+  });
+}
+
+class Wizard {
+  String name;
+  int hp;
+  Wand? wand;
+
+  Wizard({
+    required this.name,
+    required this.hp,
+    this.wand,
+  });
+}
