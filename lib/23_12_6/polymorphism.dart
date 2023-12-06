ee2982874f58a3d9c690bea19d0574d21fa97ca8
gminiy <gmini.y@gmail.com>
2023-12-06 14:11:59 +0900

    연습문제 13-2 다형성

---
+++ lib/23_12_6/polymorphism.dart
@@ -0,0 +1,52 @@
+abstract interface class X {
+  void a();
+}
+
+abstract class Y implements X {
+  void b();
+}
+
+class A extends Y {
+  @override
+  void a() {
+    print('Aa');
+  }
+
+  @override
+  void b() {
+    print('Ab');
+  }
+
+  void c() {
+    print('Ac');
+  }
+}
+
+class B extends Y {
+  @override
+  void a() {
+    print('Ba');
+  }
+
+  @override
+  void b() {
+    print('Bb');
+  }
+
+  void c() {
+    print('Bc');
+  }
+}
+
+void main() {
+  X obj = A();
+  obj.a();
+  // obj.b(); X에 없어서 안됨
+  // obj.c(); X에 없어서 안됨
+
+  Y y1 = A();
+  Y y2 = B();
+
+  y1.a(); //'Aa' 출력
+  y2.a(); //'Ba' 출력
+}
