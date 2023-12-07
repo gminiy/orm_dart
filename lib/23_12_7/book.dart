b8ca711f8d30991948a73274548eeb32858e35b6
gminiy <gmini.y@gmail.com>
2023-12-07 16:36:12 +0900

    연습문제

---
+++ lib/23_12_7/book.dart
@@ -0,0 +1,97 @@
+class Book {
+  String title;
+  DateTime publishDate = DateTime.now();
+  String comment;
+
+  Book({
+    required this.title,
+    required this.publishDate,
+    required this.comment,
+  });
+
+// 제목과 출간일이 같으면 같은 책으로 판단한다. 또한 List, Set, Map 등의 컬렉션에 넣어도 동일 객체로 판단한다.
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        other is Book &&
+            runtimeType == other.runtimeType &&
+            publishDate == other.publishDate;
+  }
+
+  // hashCode도 함께 오버라이드
+  @override
+  int get hashCode => publishDate.hashCode;
+
+  // deep copy 를 지원한다
+  Book copyWith({
+    String? title,
+    DateTime? publishDate,
+    String? comment,
+  }) {
+    return Book(
+      title: title ?? this.title,
+      publishDate: publishDate ?? this.publishDate,
+      comment: comment ?? this.comment,
+    );
+  }
+
+  @override
+  String toString() {
+    return 'Book{title: $title, publishDate: $publishDate, comment: $comment}';
+  }
+}
+
+void main() {
+  final DateTime now = DateTime.now();
+  final DateTime oneDayAfter = now.add(Duration(days: 1));
+  final DateTime twoDayAfter = now.add(Duration(days: 2));
+  final Book book1 = Book(
+    title: 'title1',
+    publishDate: now,
+    comment: 'comment1',
+  );
+  final Book book2 = Book(
+    title: 'title1-1',
+    publishDate: now,
+    comment: 'comment1-1',
+  );
+  final Book book3 = Book(
+    title: 'title2',
+    publishDate: twoDayAfter,
+    comment: 'comment2',
+  );
+  final Book book4 = Book(
+    title: 'title3',
+    publishDate: oneDayAfter,
+    comment: 'comment3',
+  );
+
+  // 제목과 출간일이 같으면 같은 책으로 판단한다. 또한 List, Set, Map 등의 컬렉션에 넣어도 동일 객체로 판단한다.
+  List<Book> bookList = [book1, book2, book3, book4];
+  Set<Book> bookSet = {book1, book2};
+  Map<String, Book> bookMap = {
+    "book1": book1,
+    "book2": book2,
+  };
+  print(book1 == book2);
+  print('${bookList[0] == bookList[1]}');
+
+  print('${bookSet.length}');
+  print(bookSet);
+
+  print('${bookMap['book1'] == bookMap['book2']}');
+
+  //Book 인스턴스를 담고 있는 컬렉션에 대해 sort() 를 수행하여 출간일이 오래된 순서대로 정렬한다.
+  bookList.sort((a, b) => a.publishDate.millisecondsSinceEpoch
+      .compareTo(b.publishDate.millisecondsSinceEpoch));
+
+  bookList.forEach((element) {
+    print(element.title);
+  }); // result: title1, title1-1, title3, title2
+
+  final Book originBook =
+      Book(title: "origin", publishDate: now, comment: "origin book comment");
+  final Book copyBook = originBook.copyWith();
+  print('origin: $originBook');
+  print('copy: $copyBook');
+}
