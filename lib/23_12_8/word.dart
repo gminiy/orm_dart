8d2b3adb8462db4f22c0e369bdee2c65863a32ba
gminiy <gmini.y@gmail.com>
2023-12-08 17:43:46 +0900

    실습 1

---
+++ lib/23_12_8/word.dart
@@ -0,0 +1,23 @@
+class Word {
+  String word = 'vowEl';
+  List<String> vowels = ['a', 'e', 'i', 'o', 'u'];
+
+  bool isVowel(int i) {
+    final String selectedChar =word.substring(i, i + 1);
+    print(selectedChar);
+
+    return vowels.contains(selectedChar.toLowerCase());
+  }
+}
+
+void main() {
+  final Word word = Word();
+  // lowercase test
+  print(word.isVowel(1));
+  // uppercase test
+  print(word.isVowel(3));
+  // consonant test
+  print(word.isVowel(0));
+  print(word.isVowel(2));
+  print(word.isVowel(4));
+}
