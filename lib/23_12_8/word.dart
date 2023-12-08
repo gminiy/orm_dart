10687761fdb7e65854a9ac076138bb6ae80484b0
gminiy <gmini.y@gmail.com>
2023-12-08 17:46:39 +0900

    실습 2

--- lib/23_12_8/word.dart
+++ lib/23_12_8/word.dart
@@ -1,5 +1,5 @@
 class Word {
-  String word = 'vowEl';
+  String word = 'VowEl';
   List<String> vowels = ['a', 'e', 'i', 'o', 'u'];
 
   bool isVowel(int i) {
@@ -8,10 +8,16 @@ class Word {
 
     return vowels.contains(selectedChar.toLowerCase());
   }
+
+  bool isConsonant(int i) {
+    return !isVowel(i);
+  }
 }
 
 void main() {
   final Word word = Word();
+
+  print('isVowel Test');
   // lowercase test
   print(word.isVowel(1));
   // uppercase test
@@ -20,4 +26,14 @@ void main() {
   print(word.isVowel(0));
   print(word.isVowel(2));
   print(word.isVowel(4));
+
+  print('isConsonant Test');
+  // lowercase test
+  print(word.isConsonant(1));
+  // uppercase test
+  print(word.isConsonant(3));
+  // consonant test
+  print(word.isConsonant(0));
+  print(word.isConsonant(2));
+  print(word.isConsonant(4));
 }
