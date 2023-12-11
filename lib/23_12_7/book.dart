e4fd859cc8f33eee36566b81785949fc3176979e
gminiy <gmini.y@gmail.com>
2023-12-11 09:10:40 +0900

    같은 책인지 여부를 Date로 변경

--- lib/23_12_7/book.dart
+++ lib/23_12_7/book.dart
@@ -1,7 +1,10 @@
+import 'package:intl/intl.dart';
+
 class Book {
   String title;
   DateTime publishDate = DateTime.now();
   String comment;
+  final DateFormat formatter = DateFormat('yyyy-MM-dd');
 
   Book({
     required this.title,
@@ -15,13 +18,13 @@ class Book {
     return identical(this, other) ||
         other is Book &&
             runtimeType == other.runtimeType &&
-            publishDate == other.publishDate &&
+            formatter.format(publishDate) == formatter.format(other.publishDate) &&
             title == other.title;
   }
 
   // hashCode도 함께 오버라이드
   @override
-  int get hashCode => publishDate.hashCode ^ title.hashCode;
+  int get hashCode => formatter.format(publishDate).hashCode ^ title.hashCode;
 
   // deep copy 를 지원한다
   Book copyWith({
@@ -68,21 +71,20 @@ void main() {
   );
 
   // 제목과 출간일이 같으면 같은 책으로 판단한다. 또한 List, Set, Map 등의 컬렉션에 넣어도 동일 객체로 판단한다.
-  List<Book> bookList = [book1, book2, book3, book4];
-  Set<Book> bookSet = {book1, book2};
-  Map<String, Book> bookMap = {
-    "book1": book1,
-    "book2": book2,
-  };
-  print(book1 == book2);
-  print('${bookList[0] == bookList[1]}');
+  final Book book5 = Book(
+    title: 'title1',
+    publishDate: DateTime.now(),
+    comment: 'comment1',
+  );
+  Set<Book> bookSet = {book1, book5};
+
 
   print('${bookSet.length}');
   print(bookSet);
 
-  print('${bookMap['book1'] == bookMap['book2']}');
 
   //Book 인스턴스를 담고 있는 컬렉션에 대해 sort() 를 수행하여 출간일이 오래된 순서대로 정렬한다.
+  List<Book> bookList = [book1, book2, book3, book4];
   bookList.sort((a, b) => a.publishDate.millisecondsSinceEpoch
       .compareTo(b.publishDate.millisecondsSinceEpoch));
 
