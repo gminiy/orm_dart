160f833cdee83653e3b4359a12933b5d31927ffc
gminiy <gmini.y@gmail.com>
2023-12-13 16:03:35 +0900

    연습문제 copy 비동기

---
+++ lib/23_12_13/copy.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main() async {
+  final String currentDir = Directory(Platform.script.toFilePath()).parent.path;
+  final sampleFile = File('$currentDir/smple.csv');
+  final copyFile = File('$currentDir/copy.csv');
+  final sampleFileString = await sampleFile.readAsString().catchError((e) {
+    print(e);
+
+    return '';
+  });
+  final replaced = sampleFileString.replaceAll('한석봉', '김석봉');
+
+  await copyFile.writeAsString(replaced);
+}
